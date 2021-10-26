d3f0aaa37616a5dfe827979d1a0edd45cdc4e2e8
Nguyễn Văn Liêm - 60135923 <nvliemc4ncthanh@gmail.com>
2021-10-26 13:10:34 +0700

    Update qlnhanvien.sql

--- Lab6/qlnhanvien.sql
+++ Lab6/qlnhanvien.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2021 at 05:16 AM
+-- Generation Time: Oct 26, 2021 at 08:03 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
